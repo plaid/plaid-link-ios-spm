89057494f89d3ef21c8dcac4ab753985b3556712
dtroupe <dtroupe@plaid.com>
2024-08-02 16:12:20 -0700

    initial setup

---
+++ LinkKit/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+/// A precompiled XCFramework of the plaid-link-ios repo that was compiled with Xcode 15.0.1
+/// This XCFramework can be used by Xcode 15.0.1 and later.
+/// "https://github.com/plaid/plaid-link-ios/archive/refs/tags/5.6.0/LinkKit.xcframework.zip",
+let linkKitXCFramework = Target.binaryTarget(
+  name: "LinkKit",
+  url: "https://github.com/plaid/plaid-link-ios/archive/refs/tags/5.6.0.zip",
+  checksum: "76d63cbf94a79525fa6a926bbfc7566d6d7038de61e71b74a2b6d69052aec391"
+)
+
+let package = Package(
+  name: "LinkKit",
+  platforms: [.iOS(.v14)],
+  products: [.library(name: "LinkKit", targets: ["LinkKit", "LinkKitSub"])],
+  targets: [
+    linkKitXCFramework,
+
+    // Without at least one regular (non-binary) target, this package doesn't show up
+    // in Xcode under "Frameworks, Libraries, and Embedded Content". That prevents
+    // Lottie from being embedded in the app product, causing the app to crash when
+    // ran on a physical device. As a workaround, we can include a stub target
+    // with at least one source file.
+    // https://github.com/apple/swift-package-manager/issues/6069
+    .target(name: "LinkKitSub", path: "Sources"),
+  ]
+)
