dcdc483b783eeb1c5881a78ec25dbbde28e4deb8
dtroupe <dtroupe@plaid.com>
2024-08-05 10:33:53 -0700

    v0.0.1

--- LinkKit/Package.swift
+++ LinkKit/Package.swift
@@ -3,10 +3,9 @@ import PackageDescription
 
 /// A precompiled XCFramework of the plaid-link-ios repo that was compiled with Xcode 15.0.1
 /// This XCFramework can be used by Xcode 15.0.1 and later.
-/// "https://github.com/plaid/plaid-link-ios/archive/refs/tags/5.6.0/LinkKit.xcframework.zip",
 let linkKitXCFramework = Target.binaryTarget(
   name: "LinkKit",
-  url: "https://github.com/plaid/plaid-link-ios/archive/refs/tags/5.6.0.zip",
+  url: "https://github.com/plaid/plaid-link-ios/archive/refs/tags/5.6.0/LinkKit.xcframework.zip",
   checksum: "76d63cbf94a79525fa6a926bbfc7566d6d7038de61e71b74a2b6d69052aec391"
 )
 
@@ -23,6 +22,6 @@ let package = Package(
     // ran on a physical device. As a workaround, we can include a stub target
     // with at least one source file.
     // https://github.com/apple/swift-package-manager/issues/6069
-    .target(name: "LinkKitSub", path: "Sources"),
+    .target(name: "LinkKitSub", path: "Sources", resources: [.copy("Resources/PrivacyInfo.xcprivacy")]),
   ]
 )
