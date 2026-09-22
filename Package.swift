bf102f2e55e2d21b9bd35c90e1bafdae6c39f8c5
dtroupe <dtroupe@plaid.com>
2026-09-22 09:30:43 -0700

    update to LinkKit 6.5.2

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 /// This XCFramework can be used by Xcode 16.1.0 and later.
 let linkKitXCFramework = Target.binaryTarget(
   name: "LinkKit",
-  url: "https://github.com/plaid/plaid-link-ios/releases/download/6.5.1/LinkKit.xcframework.zip",
-  checksum: "d2b8877e214c58fe9b6382bd28cbf9debdda5e5bef0382417d6af1e38e9716c6"
+  url: "https://github.com/plaid/plaid-link-ios/releases/download/6.5.2/LinkKit.xcframework.zip",
+  checksum: "e9b637bd7dfedac0a3bf841c9fbbcc0cd919bc0b642f426b1393dfb515ef77e0"
 )
 
 let package = Package(
